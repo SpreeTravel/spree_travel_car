e1d66a741fa5937ccd17f40793d2321b90bac00d
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2016-01-22 16:16:05 -0500

    defining calculation formula for the travel products that are in sample gem

--- app/models/spree/calculator_car.rb
+++ app/models/spree/calculator_car.rb
@@ -1,9 +1,71 @@
 module Spree
   class CalculatorCar < BaseCalculator
+    # def adults_range
+    #   (0..1).to_a
+    # end
+    #
+    # def children_range
+    #   (0..1).to_a
+    # end
 
-    def calculate_price(context, variant, options)
+    def calculate_price(context, product, variant, options)
+      # TODO hacerlo generico para que se apte cuando hay cambios en los context por el usuario
+      return [product.price.to_f] if product.rates.empty?
+
+      # for productos
+      # list = Spree::Variant.where(product: product.id).includes(:option_values).where(spree_option_values: {id: context.category(options).to_i}).collect{|v|v.rates}.flatten
+
+      # for variantes
+      list = variant.rates
+
+      array = []
+      list.each do |r|
+        if Date.parse(r.start_date) <= Date.parse(context.pickup_date(options).to_s) &&
+           Date.parse(r.end_date) >= Date.parse(context.return_date(options).to_s)
+
+          days = (Date.parse(context.return_date(options).to_s) - Date.parse(context.pickup_date(options).to_s)).to_i
+
+          if days >= 3 and days <= 6
+            price = days * r.three_six_days
+            array << {price: price, rate: r.id, avg: nil}
+          elsif days >= 7 and days <= 13
+            price = days * r.seven_thirteen_days
+            array << {price: price, rate: r.id, avg: nil}
+          elsif  days >= 14 and days <=29
+            price = days * r.fourteen_tuentynine_days
+            array << {price: price, rate: r.id, avg: nil}
+          end
+        end
+      end
+      array
 
     end
 
+
+    #
+    # def get_rate_price(rate, adults, children)
+    #   adults = adults.to_i
+    #   children = children.to_i
+    #   price = adults * rate.one_adult.to_i + children * rate.one_child.to_i
+    #   price
+    # end
+
+    # private
+
+    # def get_adult_list(rate, pt_adults)
+    #   if pt_adults.present?
+    #     [pt_adults]
+    #   else
+    #     adults_range
+    #   end
+    # end
+    #
+    # def get_child_list(rate, pt_child)
+    #   if pt_child.present?
+    #     [pt_child]
+    #   else
+    #     children_range
+    #   end
+    # end
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,5 +4,10 @@ module Spree
     def car?
       self.product_type == Spree::ProductType.find_by_name('car')
     end
+    
+    def self.cars
+      where(product_type_id: Spree::ProductType.find_by_name('car').id )
+    end
+    
   end
 end
