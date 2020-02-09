4d856ecb52e1a4161a84b00fdce39e059fc1f875
Raul <raul@backlotcars.com>
2020-02-09 17:59:07 -0300

    Why: Via the prepending a module into a class the class methods where not loaded then could not be used Change: Prepending a module with a class method to the singleton class of a class

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,13 +1,15 @@
-module Spree
-  Product.class_eval do
-
-    def car?
-      self.product_type == Spree::ProductType.find_by_name('car')
-    end
-    
-    def self.cars
-      where(product_type_id: Spree::ProductType.find_by_name('car').id )
-    end
-    
+module Spree::ProductDecorator
+  def car?
+    self.product_type == Spree::ProductType.find_by_name('car')
   end
 end
+
+Spree::Product.prepend Spree::ProductDecorator
+
+module Spree::ProductDecoratorClassMethod
+  def cars
+    where(product_type_id: Spree::ProductType.find_by_name('car').id )
+  end
+end
+
+Spree::Product.singleton_class.send :prepend, Spree::ProductDecoratorClassMethod
