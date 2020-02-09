ed2201f599deb2d6ec59eac472659573d4b46eae
Raul <raul@backlotcars.com>
2020-02-09 18:02:33 -0300

    Style fix, typos and prepending methods

--- app/models/spree/rate_decorator.rb
+++ app/models/spree/rate_decorator.rb
@@ -1,15 +1,17 @@
-Spree::Rate.class_eval do
+module Spree::RateDecorator
 
     def three_six_days
       get_persisted_option_value(:three_six_days).to_i
     end
 
     def seven_thirteen_days
-      get_persisted_option_value(:three_six_days).to_i
+      get_persisted_option_value(:seven_thirteen_days).to_i
     end
 
-    def fourteen_tuentynine_days
-      get_persisted_option_value(:three_six_days).to_i
+    def fourteen_twentynine_days
+      get_persisted_option_value(:fourteen_twentynine_days).to_i
     end
 
 end
+
+Spree::Rate.prepend Spree::RateDecorator
