75cff2a0aeed3b2bde5b3b442ceefa55580a39cb
Raul <raul@backlotcars.com>
2020-06-09 19:09:26 -0300

    Why: we need to return the variant

--- app/models/spree/calculator_car.rb
+++ app/models/spree/calculator_car.rb
@@ -4,24 +4,24 @@ module Spree
     def calculate_price(context, product, variant, options)
       return [price:product.price.to_f] if product.rates.empty?
 
-      list = variant.rates
+      rates = variant.rates
 
       array = []
-      list.each do |r|
-        if Date.parse(r.start_date) <= Date.parse(context.pickup_date(options).to_s) &&
-            Date.parse(r.end_date) >= Date.parse(context.return_date(options).to_s)
+      rates.each do |rate|
+        if Date.parse(rate.start_date) <= Date.parse(context.pickup_date(options).to_s) &&
+            Date.parse(rate.end_date) >= Date.parse(context.return_date(options).to_s)
 
           days = (Date.parse(context.return_date(options).to_s) - Date.parse(context.pickup_date(options).to_s)).to_i
 
           price = if days >= 3 and days <= 6
-                    days * r.three_six_days
+                    days * rate.three_six_days
                   elsif days >= 7 and days <= 13
-                    days * r.seven_thirteen_days
+                    days * rate.seven_thirteen_days
                   elsif  days >= 14 and days <=29
-                    days * r.fourteen_twentynine_days
+                    days * rate.fourteen_twentynine_days
                   end
 
-          array << {price: price.format, rate: r.id, avg: nil}
+          array << {price: price.format, rate: rate.id, avg: nil, variant: rate.variant}
 
         end
       end
