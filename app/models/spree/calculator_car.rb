be52c7fca4570cf6953235fb055a494914e464a6
Raul <raul@backlotcars.com>
2020-07-24 15:27:53 -0300

    Why: making primary function smaller by refactoring and dividing to smaller functions

--- app/models/spree/calculator_car.rb
+++ app/models/spree/calculator_car.rb
@@ -1,33 +1,52 @@
+# frozen_string_literal: true.
+
 module Spree
   class CalculatorCar < BaseCalculator
+    def initialize(context:, product:, variant:, options:)
+      @product = product
+      @variant = variant
+      @options = options
+      @context_pickup_date = Date.parse(context.pickup_date(options).to_s)
+      @context_return_date = Date.parse(context.return_date(options).to_s)
+    end
 
-    def calculate_price(context, product, variant, options)
-      return [price:product.price.to_f] if product.rates.empty?
+    def calculate_price
+      return [price: product.price.to_f] if product.rates.empty?
 
       rates = variant.rates
 
       array = []
       rates.each do |rate|
-        if Date.parse(rate.start_date) <= Date.parse(context.pickup_date(options).to_s) &&
-            Date.parse(rate.end_date) >= Date.parse(context.return_date(options).to_s)
+        next unless valid_dates?(rate)
 
-          days = (Date.parse(context.return_date(options).to_s) - Date.parse(context.pickup_date(options).to_s)).to_i
+        days = (context_return_date - context_pickup_date).to_i
 
-          price = if days >= 3 and days <= 6
-                    days * rate.three_six_days
-                  elsif days >= 7 and days <= 13
-                    days * rate.seven_thirteen_days
-                  elsif  days >= 14 and days <=29
-                    days * rate.fourteen_twentynine_days
-                  end
+        price = fetch_price(days, rate)
 
-          array << {price: price.format, rate: rate.id, avg: nil, variant: rate.variant}
-
-        end
+        array << { price: price.format, rate: rate.id, avg: nil, variant: rate.variant }
       end
       array
-
     end
 
+    private
+
+    attr_reader :product, :variant, :options, :context_pickup_date, :context_return_date
+
+    def valid_dates?(rate)
+      Date.parse(rate.start_date) <= context_pickup_date &&
+        Date.parse(rate.end_date) >= context_return_date
+    end
+
+    def fetch_price(days, rate)
+      rate_per_day = if days >= 3 && days <= 6
+                       rate.three_six_days
+                     elsif days >= 7 && days <= 13
+                       rate.seven_thirteen_days
+                     elsif days >= 14 && days <= 29
+                       rate.fourteen_twentynine_days
+                     end
+
+      days * rate_per_day
+    end
   end
 end
