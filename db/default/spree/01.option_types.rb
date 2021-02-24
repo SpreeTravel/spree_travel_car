8b5ea5ad27f2cc25294561fed98a644a10a84054
Raul <raul@backlotcars.com>
2021-02-24 18:23:53 -0300

    Why: As part of the StrategyPattern initial implementation, each attr type will have a different way to build and return attributes. This is a work in progres and may have more steps

--- db/default/spree/01.option_types.rb
+++ db/default/spree/01.option_types.rb
@@ -2,16 +2,16 @@
 option_types = [
   {name: "start_date", presentation:  "Start Date", attr_type: 'date', travel: true, preciable: false },
   {name: "end_date",   presentation:  "End Date",   attr_type: 'date', travel: true, preciable: false },
-  {name: 'three_six_days',   presentation:  "3 to 6 days",   attr_type: 'integer', travel: true, preciable: true},
-  {name: 'seven_thirteen_days',  presentation:  "7 to 13 days",  attr_type: 'integer', travel: true, preciable: true },
-  {name: 'fourteen_twentynine_days', presentation:  "14 to 29 days", attr_type: 'integer', travel: true, preciable: true },
+  {name: 'three_six_days',   presentation:  "3 to 6 days",   attr_type: 'price', travel: true, preciable: true},
+  {name: 'seven_thirteen_days',  presentation:  "7 to 13 days",  attr_type: 'price', travel: true, preciable: true },
+  {name: 'fourteen_twentynine_days', presentation:  "14 to 29 days", attr_type: 'price', travel: true, preciable: true },
   {name: "category",   presentation:  "Category",     attr_type: 'selection', travel: true, preciable: false },
   {name: "transmision", presentation:  "Transmision", attr_type: 'selection', travel: true, preciable: false },
   {name: "pickup_date", presentation:  "Pickup Date", attr_type: 'date', travel: true, preciable: false },
   {name: "return_date", presentation:  "Return Date", attr_type: 'date', travel: true, preciable: false },
   {name: "pickup_destination", presentation:  "Pickup Destination", attr_type: 'destination', travel: true, preciable: false },
   {name: "return_destination", presentation:  "Return Destination", attr_type: 'destination', travel: true, preciable: false },
-  {name: "adult", presentation:  "Adult", attr_type: 'integer', short:'Adult', travel: true, preciable: false },
+  {name: "adult", presentation:  "Adult", attr_type: 'pax', short:'Adult', travel: true, preciable: false },
 ]
 
 ### Creating Option Types
