b86cbedb527f87c49eb2505ff5e4fbf09569a5bc
Raul <raul@backlotcars.com>
2021-06-03 23:36:20 -0300

    Why: Having this query in the calculator gives this model too much responsability, so it is separated in a query service and instantiated from the calculator

--- app/models/spree/calculator_car.rb
+++ app/models/spree/calculator_car.rb
@@ -12,11 +12,6 @@ module Spree
     def calculate_price
       return [price: variant.product.price.to_f] if variant.rates.empty?
 
-      start_date = Spree::OptionValue.select(:id).find_by(name: 'start_date')
-      end_date = Spree::OptionValue.select(:id).find_by(name: 'end_date')
-
-      rate = fetch_query(start_date, end_date)
-
       days = (context_return_date - context_pickup_date).to_i
 
       variant.context_price = fetch_price(days, rate)
@@ -25,16 +20,18 @@ module Spree
 
     private
 
-    attr_reader :product, :variant, :options, :context_pickup_date, :context_return_date
+    def rate
+      @start_date ||= Spree::OptionValue.select(:id).find_by(name: 'start_date')
+      @end_date ||= Spree::OptionValue.select(:id).find_by(name: 'end_date')
 
-    def fetch_query(start_date, end_date)
-      joins_clause = "INNER JOIN spree_rate_option_values as rot1 ON rot1.rate_id = spree_rates.id AND rot1.option_value_id = #{start_date.id}" \
-                      "INNER JOIN spree_rate_option_values as rot2 ON rot2.rate_id = spree_rates.id AND rot2.option_value_id = #{end_date.id}"
-      where_clause = "spree_rates.variant_id = ? and rot1.date_value <= ? and rot2.date_value >= ?"
-
-      Spree::Rate.joins(joins_clause)
-                 .where(where_clause, variant.id, context_pickup_date, context_return_date).take
+      @rate ||= Spree::FindRates.new(variant_id: variant.id,
+                                     start_date_option_value_id: @start_date.id,
+                                     end_date_option_value_id: @end_date.id,
+                                     context_pickup_date: context_pickup_date,
+                                     context_return_date: context_return_date).execute
     end
+
+    attr_reader :product, :variant, :options, :context_pickup_date, :context_return_date
 
     def fetch_price(days, rate)
       rate_per_day = if days >= 3 && days <= 6
